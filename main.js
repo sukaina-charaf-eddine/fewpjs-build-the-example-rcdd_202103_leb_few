1a29195e69afdc90889761590826c22f1d98053b
Sukaina Charaf Eddine <sukaina.kabalan01@gmail.com>
2021-04-30 09:57:19 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+
 const modal = document.getElementById('modal')
 const likeButtons = document.getElementsByClassName('like-glyph')
 
